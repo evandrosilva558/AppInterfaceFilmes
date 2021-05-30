7e80803db7971ffe1f83f8ab50b9de0f6afceed9
evandrosilva558 <evandrosilva558@hotmail.com>
2021-05-30 18:38:20 -0300

    criado o carrosel

--- App.js
+++ App.js
@@ -7,9 +7,12 @@ import { View,
   Dimensions, 
   ImageBackground, 
   TextInput, 
-  TouchableOpacity } from 'react-native';
+  TouchableOpacity, 
+  Image} from 'react-native';
 
 import { FontAwesome } from '@expo/vector-icons';
+
+import ViewPager from '@react-native-community/viewpager';
 
 const { width: screenWidth, height: screenHeight } = Dimensions.get('window');
 
@@ -56,6 +59,81 @@ export default function App() {
   // filmes iniciando na posição "0" do array.
   const [background, setBackground] = useState(lista[0].img)
 
+  const MyPager = ({item, index}) => {
+    return (
+      <View style={{ flex: 1 }}>
+        <ViewPager style={styles.viewPager} initialPage={0}>
+
+          <View style={styles.carouselImg} key="0">
+            <TouchableOpacity>
+            
+            <Image
+            style={styles.viewImage}
+            source={{uri: 'https://sujeitoprogramador.com/wp-content/uploads/2020/05/background.jpg'}}
+            />
+            </TouchableOpacity>
+            <Text style={styles.carouselText}>primeiro</Text>
+          </View>
+
+          <View style={styles.carouselImg} key="1">
+          <TouchableOpacity>
+            
+            <Image
+            style={styles.viewImage}
+            source={{uri: 'https://sujeitoprogramador.com/wp-content/uploads/2020/05/badboy.jpg'}}
+            />
+            </TouchableOpacity>
+            <Text style={styles.carouselText}>segundo</Text>
+          </View>
+
+          <View style={styles.carouselImg} key="2">
+          <TouchableOpacity>
+            
+            <Image
+            style={styles.viewImage}
+            source={{uri: 'https://sujeitoprogramador.com/wp-content/uploads/2020/05/blackwidow.jpg'}}
+            />
+            </TouchableOpacity>
+            <Text style={styles.carouselText}>terceiro</Text>
+          </View>
+
+          <View style={styles.carouselImg} key="3">
+          <TouchableOpacity>
+            
+            <Image
+            style={styles.viewImage}
+            source={{uri: 'https://sujeitoprogramador.com/wp-content/uploads/2020/05/topgun.jpeg'}}
+            />
+            </TouchableOpacity>
+            <Text style={styles.carouselText}>quarto</Text>
+          </View>
+
+          <View style={styles.carouselImg} key="4">
+          <TouchableOpacity>
+            
+            <Image
+            style={styles.viewImage}
+            source={{uri: 'https://sujeitoprogramador.com/wp-content/uploads/2020/05/blood.jpg'}}
+            />
+            </TouchableOpacity>
+            <Text style={styles.carouselText}>quinto</Text>
+          </View>
+
+          <View style={styles.carouselImg} key="5">
+          <TouchableOpacity>
+            
+            <Image
+            style={styles.viewImage}
+            source={{uri: 'https://sujeitoprogramador.com/wp-content/uploads/2020/05/freeguy.jpg'}}
+            />
+            </TouchableOpacity>
+            <Text style={styles.carouselText}>sexto</Text>
+          </View>
+
+        </ViewPager>
+      </View>
+    );
+  };
   return (
     <ScrollView style={styles.container}>
       <View style={{flex: 1, height: screenHeight}}>
@@ -78,11 +156,8 @@ export default function App() {
             <Text style={styles.titulo}>
               Lançamentos recentes !
             </Text>
-
-            <View style={styles.slideView}> 
-
-            </View>
-
+            <MyPager/>
+            
           </ImageBackground>
         </View>
       </View>
@@ -133,10 +208,41 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
   },
   slideView: {
+    flex: 1,
     width: '100%',
     height: 350,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  page: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  viewPager: {
+    flex: 1,
+  },
+  page: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  carouselImg: {
+    alignSelf: 'center',
+    width: 250,
+    height: 350,
+    borderRadius: 12,
+  },
+  viewImage: {
+    width: 200,
+    height: 300,
+    borderRadius: 7,
+    alignSelf: 'center',
+  },
+  carouselText: {
+    alignSelf: 'center',
+    margin: 10,
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#fff'
   }
-
+  
 });
